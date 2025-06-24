2dd3ca584f1a0e348d8015a75d346a494c2efead
Birgiman <eduardo.birgiman@outlook.com>
2025-06-24 17:32:21 -0300

    Aplicando limite máximo de caracters no formulário

--- frontend/src/schemas/ContactFormSchema.ts
+++ frontend/src/schemas/ContactFormSchema.ts
@@ -6,16 +6,18 @@ export const contactFormSchema = z.object({
   user:
     z.string()
     .min(2, 'Usuário deve conter no mínimo 2 letras')
+    .max(32, 'Usuário deve conter no máximo 2 letras')
     .regex(regex.lettersOnly, messages.lettersOnly),
 
-    displayName:
-      z.string()
-      .min(2, 'Apelido deve conter no mínimo 2 caracteres')
-      .regex(regex.displayName, messages.displayName),
+  displayName:
+    z.string()
+    .min(2, 'Apelido deve conter no mínimo 2 caracteres')
+    .max(40, 'Usuário deve conter no máximo 40 letras')
+    .regex(regex.displayName, messages.displayName),
 
-    cep:
-      z.string()
-      .regex(regex.cep, messages.cepInvalid)
+  cep:
+    z.string()
+    .regex(regex.cep, messages.cepInvalid)
 })
 
 export type ContactFormSchema = z.infer<typeof contactFormSchema>
